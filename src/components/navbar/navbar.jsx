541c3875a3da04489ca518849833c599124f94bc
Saravanank2005 <saravanan.k2023it@sece.ac.in>
2025-01-30 14:25:15 +0530

    first commit

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import './Navbar.css';
-import logo from '../assets/logo.png';
+
 import { Link, useNavigate } from 'react-router-dom';
 import { useUserAuth } from '../../context/UserAuthContext';
 import { ShopContext } from '../../context/ShopContext';
